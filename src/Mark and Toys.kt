6174cf216f8de37026fba36a5128e6d5683d323e
Ivan Karpiuk <ivan.karpiuk@mcpk.tech>
2020-04-29 23:07:56 +0300

    Mark and Toys: solution

---
+++ src/Mark and Toys.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+// Complete the maximumToys function below.
+fun maximumToys(prices: Array<Int>, k: Int): Int {
+    prices.sort()
+    var moneyLeft = k
+    var toysCount = 0
+    var i = 0
+    while (moneyLeft > 0) {
+        if (moneyLeft - prices[i] >= 0) {
+            moneyLeft -= prices[i]
+            toysCount++
+            i++
+        } else break
+    }
+    return toysCount
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nk = scan.nextLine().split(" ")
+
+    val n = nk[0].trim().toInt()
+
+    val k = nk[1].trim().toInt()
+
+    val prices = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = maximumToys(prices, k)
+
+    println(result)
+}
