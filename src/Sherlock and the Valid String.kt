700d9f0ecd6a2480c1e1b82cdba414b6733b8087
Ivan Karpiuk <ivan.karpiuk@mcpk.tech>
2020-05-03 20:58:12 +0300

    Sherlock and the Valid String: solution

---
+++ src/Sherlock and the Valid String.kt
@@ -0,0 +1,51 @@
+import java.util.*
+
+// Complete the isValid function below.
+fun isValid(s: String): String {
+    val count = IntArray(26) { 0 }
+    for (c in s)
+        count[c - 'a']++
+    var first = -1
+    var firstCount = 0
+    var second = -1
+    var secondCount = 0
+
+    for (i in count) {
+        if (i != 0) {
+            if (first == -1 || first == i) {
+                first = i
+                firstCount++
+            } else if (second == -1 || second == i) {
+                second = i
+                secondCount++
+            } else {
+                return "NO"
+            }
+        }
+    }
+    return when {
+        second == -1 -> "YES"
+        first > second -> if (firstCount == 1 && first - second == 1)
+            "YES"
+        else if (secondCount == 1)
+            "YES"
+        else
+            "NO"
+        else -> if (secondCount == 1 && second - first == 1)
+            "YES"
+        else if (firstCount == 1)
+            "YES"
+        else
+            "NO"
+    }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val s = scan.nextLine()
+
+    val result = isValid(s)
+
+    println(result)
+}
