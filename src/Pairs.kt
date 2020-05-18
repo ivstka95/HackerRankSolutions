864ce9edf5ded5861ac113befca38f8d2ce07b8d
Ivan Karpiuk <ivan.karpiuk@mcpk.tech>
2020-05-18 16:15:00 +0300

    solutions

---
+++ src/Pairs.kt
@@ -0,0 +1,43 @@
+import java.util.*
+
+// Complete the pairs function below.
+fun pairs(k: Int, arr: Array<Int>): Int {
+    var arr = arr.sortedArray()
+    var i = 0
+    var j = 1
+    var count = 0
+
+    while (j < arr.size) {
+        val diff = arr[j] - arr[i]
+
+        when {
+            diff == k -> {
+                count++
+                j++
+            }
+            diff > k -> {
+                i++
+            }
+            diff < k -> {
+                j++
+            }
+        }
+    }
+    return count
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nk = scan.nextLine().split(" ")
+
+    val n = nk[0].trim().toInt()
+
+    val k = nk[1].trim().toInt()
+
+    val arr = scan.nextLine().trim().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = pairs(k, arr)
+
+    println(result)
+}
---
+++ src/Triple sum.kt
@@ -0,0 +1,47 @@
+import java.util.*
+
+// Complete the triplets function below.
+fun triplets(a: Array<Int>, b: Array<Int>, c: Array<Int>): Long {
+    var a = a.toSet().toIntArray().sorted()
+    var b = b.toSet().toIntArray().sorted()
+    var c = c.toSet().toIntArray().sorted()
+
+    var ai = 0
+    var bi = 0
+    var ci = 0
+    var ans: Long = 0
+
+    while (bi < b.size) {
+        while (ai < a.size && b[bi] >= a[ai])
+            ai++
+
+        while (ci < c.size && b[bi] >= c[ci])
+            ci++
+
+        ans += ai.toLong() * ci.toLong()
+        bi += 1
+    }
+    return ans
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val lenaLenbLenc = scan.nextLine().split(" ")
+
+    val lena = lenaLenbLenc[0].trim().toInt()
+
+    val lenb = lenaLenbLenc[1].trim().toInt()
+
+    val lenc = lenaLenbLenc[2].trim().toInt()
+
+    val arra = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val arrb = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val arrc = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val ans = triplets(arra, arrb, arrc)
+
+    println(ans)
+}
