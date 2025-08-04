c6c7b132f0deb758e233f2261c25ae551a0be232
ivankarpiuk <ivstka95@gmail.com>
2025-08-04 12:54:58 +0400

    https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one

---
+++ src/Mini-Max Sum.kt
@@ -0,0 +1,49 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+//https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+
+fun miniMaxSum(arr: Array<Int>): Unit {
+    // Write your code here
+    var sum: Long = 0
+    var min = arr[0]
+    var max = arr[0]
+    arr.forEach { number ->
+        sum += number
+        if (number < min) {
+            min = number
+        }
+        if (number > max) {
+            max = number
+        }
+    }
+    println("${sum - max} ${sum - min}")
+}
+
+fun main(args: Array<String>) {
+
+    val arr = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    miniMaxSum(arr)
+}
