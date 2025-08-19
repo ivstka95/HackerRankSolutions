4f6da771dd44b58583deb126ecb69eb242b36b5d
ivankarpiuk <ivstka95@gmail.com>
2025-08-19 15:50:31 +0400

    https://www.hackerrank.com/challenges/three-month-preparation-kit-the-birthday-bar/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D%5B%5D=three-month-week-three

---
+++ src/Subarray Division 2.kt
@@ -0,0 +1,59 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+//https://www.hackerrank.com/challenges/three-month-preparation-kit-the-birthday-bar/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D%5B%5D=three-month-week-three
+fun birthday(s: Array<Int>, d: Int, m: Int): Int {
+    // Write your code here
+    if (m > s.size) return 0
+    var counter = 0
+    var sum = s.take(m).sum()
+    if (sum == d) counter++
+    for (i in m until s.size) {
+        sum = sum - s[i - m] + s[i]
+        if (sum == d) {
+            counter++
+        }
+    }
+    return counter
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val s = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val d = first_multiple_input[0].toInt()
+
+    val m = first_multiple_input[1].toInt()
+
+    val result = birthday(s, d, m)
+
+    println(result)
+}
